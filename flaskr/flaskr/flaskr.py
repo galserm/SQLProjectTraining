eb65fbd8609e01c8398e4f208f4414fdc96ea11c
Michael GALSERAN <michael.galseran@epitech.eu>
2017-10-29 01:14:47 +0800

    local login page and registration page

--- flaskr/flaskr/flaskr.py
+++ flaskr/flaskr/flaskr.py
@@ -18,3 +18,84 @@ def connect_db():
     rv = sqlite3.connect(app.config['DATABASE'])
     rv.row_factory = sqlite3.Row
     return rv
+
+def get_db():
+    if not hasattr(g, 'sqlite_db'):
+        g.sqlite_db = connect_db()
+    return g.sqlite_db
+
+@app.teardown_appcontext
+def close_db(error):
+    if hasattr(g, 'sqlite_db'):
+        g.sqlite_db.close()
+
+def init_db():
+    db = get_db()
+    with app.open_resource('schema.sql', mode='r') as f:
+        db.cursor().executescript(f.read())
+    db.commit()
+
+@app.cli.command('initdb')
+def initdb_command():
+    init_db()
+    print('Initialized the database.')
+
+@app.route('/')
+def show_entries():
+    db = get_db()
+    cur = db.execute('select title, text from entries order by id desc')
+    entries = cur.fetchall()
+    return render_template('show_entries.html', entries=entries)
+
+@app.route('/add', methods=['POST'])
+def add_entry():
+    if not session.get('logged_in'):
+        abort(401)
+    db = get_db()
+    db.execute('insert into entries (title, text) values (?, ?)',
+                 [request.form['title'], request.form['text']])
+    db.commit()
+    flash('New entry was successfully posted')
+    return redirect(url_for('show_entries'))
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    error = None
+    if request.method == 'POST':
+        db = get_db()
+        resp = db.execute('select username, password from users where username=? and password=?', [request.form['username'], request.form['password']])
+        entries = resp.fetchall()
+        if not entries:
+            error = 'Invalid username or password'
+        else:
+            session['logged_in'] = True
+            flash('You were logged in')
+            return redirect(url_for('show_entries'))
+    return render_template('login.html', error=error)
+
+@app.route('/logout')
+def logout():
+    session.pop('logged_in', None)
+    flash('You were logged out')
+    return redirect(url_for('show_entries'))
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    error = None
+    if request.method == 'GET':
+        return render_template('register.html')
+    if not request.form['firstname']:
+        error = 'Field marked with a * are mandatory'
+    elif not request.form['lastname']:
+        error = 'Field marked with a * are mandatory'
+    elif not request.form['username']:
+        error = 'Field marked with a * are mandatory'
+    elif not request.form['password']:
+        error = 'Field marked with a * are mandatory'
+    if error != None:
+        return render_template('register.html', error=error)
+    db = get_db()
+    db.execute("""INSERT INTO users (firstname, lastname, username, password) values (?, ?, ?, ?)""", [request.form['firstname'], request.form['lastname'], request.form['username'], request.form['password']])
+    db.commit()
+    flash('Account has been created')
+    return redirect(url_for('login'))    
--- flaskr/flaskr/schema.sql
+++ flaskr/flaskr/schema.sql
@@ -4,3 +4,12 @@ create table entries (
        title text not null,
        'text' text not null
 );
+
+drop table if exists users;
+create table users (
+       id integer primary key autoincrement,
+       firstname text not null,
+       lastname text not null,
+       username text not null,
+       password text not null
+);
